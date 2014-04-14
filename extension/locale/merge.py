b85a9d32732cc8118d918059f9bd81f688dcb7cb
Hector Zhao <l-hedgehog@126.com>
2014-04-14 21:55:03 +0800

    new strings to translate

--- extension/locale/merge.py
+++ extension/locale/merge.py
@@ -2,13 +2,16 @@
 
 def merge_file(fn):
     cn_pairs = {}
-    cn_f = open('zh-CN/%s' % fn)
-    lines = cn_f.readlines()
-    cn_f.close()
-    lines = [line for line in lines if line.strip() and not line.startswith('#')]
-    for line in lines:
-        k, v = line.split('=', 1)
-        cn_pairs[k] = v
+    try:
+        cn_f = open('zh-CN/%s' % fn)
+        lines = cn_f.readlines()
+        cn_f.close()
+        lines = [line for line in lines if line.strip() and not line.startswith('#')]
+        for line in lines:
+            k, v = line.split('=', 1)
+            cn_pairs[k] = v
+    except IOError:
+        pass
 
     en_f = open('en-US/%s' % fn)
     cn_f = open('zh-CN/%s' % fn, 'w')
@@ -26,5 +29,5 @@ def merge_file(fn):
             cn_f.write('%s=%s' % (k, v))
     cn_f.close()
 
-for fn in ['cookies.properties', 'firebug.properties', 'firebug-amo.properties', 'firebug-tracing.properties']:
+for fn in ['cookies.properties', 'firebug.properties', 'firebug-amo.properties', 'keys.properties', 'selectors.properties']:
     merge_file(fn)
